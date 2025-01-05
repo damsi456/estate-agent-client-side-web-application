8cfc565e94b27637deb3cd7e0f07711a44ef1415
Damsiha Kumarasinghe <155002269+damsi69@users.noreply.github.com>
2025-01-05 11:27:15 +0530

    Add css for tabs in property pages
    Made the page responsive as well

--- src/components/PropertyPage.jsx
+++ src/components/PropertyPage.jsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import properties from "../data/properties.json";
 
 const PropertyPage = () => {
@@ -9,20 +11,46 @@ const PropertyPage = () => {
         <div className="property-page">
             <img src={property.picture} alt={property.type} className="property-image-large"/>
             <h1>{property.location}</h1>
-            <p>
-                <b>Type:</b> {property.type}
-            </p>
-            <p>
-                <b>Price:</b> {property.price}
-            </p>
-            <p>
-                <b>Tenure:</b> {property.tenure}
-            </p>
-            <p>
-                <b>No. of Bedrooms:</b> {property.bedrooms}
-            </p>
-            <p><b>Description:</b></p>
-            <p className="property-page-description">{property.description}</p>
+            <Tabs>
+                <TabList>
+                    <Tab>Details</Tab>
+                    <Tab>Floor Plan</Tab>
+                    <Tab>Google Map</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <p>
+                        <b>Type:</b> {property.type}
+                    </p>
+                    <p>
+                        <b>Price:</b> {property.price}
+                    </p>
+                    <p>
+                        <b>Tenure:</b> {property.tenure}
+                    </p>
+                    <p>
+                        <b>No. of Bedrooms:</b> {property.bedrooms}
+                    </p>
+                    <p><b>Description:</b></p>
+                    <p className="property-page-description">{property.description}</p>
+                    </TabPanel>
+
+                <TabPanel>
+                    <img src={`/${property.id}-images/${property.id}floorplan.png`} alt={`${property.id}-floor-plan`} className="floorplan-img"/>
+                </TabPanel>
+
+                <TabPanel>
+                    <iframe
+                        src={`https://www.google.com/maps?q=${encodeURIComponent(property.location)}&output=embed`}
+                        width="100%"
+                        height="300"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        title="Google Map"
+                    ></iframe>
+                </TabPanel>
+            </Tabs>
             <p className="property-page-date"> {property.added.day}th {property.added.month}, {property.added.year}
             </p>
         </div>
